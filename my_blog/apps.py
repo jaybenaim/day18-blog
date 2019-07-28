16ece1fafba61ae037dfa668d84d3904225babd0
jaybenaim <benaimjacob@gmail.com>
2019-07-27 21:27:39 -0400

    complete

--- my_blog/apps.py
+++ my_blog/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class MyBlogConfig(AppConfig):
-    name = 'my_blog'
+    name = 'blog'
--- my_blog/models.py
+++ my_blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model): 
+    title = models.CharField(max_length=255, null=False)
+    body = models.TextField() 
+    draft = models.BooleanField(default=True)
+    published_date = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=255, null=False)
+
+
+
